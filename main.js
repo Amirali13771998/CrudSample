753e0a751bce1e06f5a52ac2b267d768be71df0a
DESKTOP-B0V8NQQ\peter <peter@gmail.com>
2023-02-12 23:04:16 -0800

    add DELETE part to my project

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.set('strictQuery', false);
 
-
 var app = express();
 var database = require('./config/database');
 var bodyParser = require('body-parser');         // pull information from HTML POST (express4)
+
+var {ObjectId}  = require('mongodb')
 
 var methodOverride = require('method-override');
 var port  = process.env.PORT || 3000;
@@ -16,21 +17,65 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
-
-
-var Employee = require('./model/employee');
+var Todo = require('./model/sample');
 mongoose.connect(database.url);
-app.listen(port);
-console.log("App listening on port : " + port);
-
-
-//get all employee data from db
-app.get('/todos', function(req, res) {
-    // use mongoose to get all todos in the database
-    Employee.find(function(err, employees) {
-        // if there is an error retrieving, send the error otherwise send data
-        if (err)
-            res.send(err)
-        res.json(employees); // return all employees in JSON format
-    });
+app.listen(port,()=>{
+    console.log("App listening on port : " + port);
 });
+
+app.post('/sample', function(req, res){
+    var todo = new Todo({
+        text : req.body.text,
+        completed : true,
+        comletedAt : 172 
+    });
+    todo.save().then((doc)=>{
+        res.send(doc)
+    },(e)=>{
+        console.log(e)
+    })
+})
+
+// app.get('/sample', (req,res)=>{
+//     Todo.find().then((todos)=>{
+//         res.send({todos})
+//     },(e)=>{
+//         res.status(400).send(e)
+//     })
+// })
+
+// app.get('/sample/:id', (req,res)=>{
+//     var id = req.params.id
+    
+//     if(!ObjectId.isValid(id)){
+//         return res.status(404).send()
+//     }
+
+//     Todo.findById(id).then((todos)=>{
+//         if(!todos){
+//             return res.status(404).send()
+//         }
+//         res.send({todos})
+//     }).catch((e)=>{
+//         res.status(400).send()
+//     })
+
+// })
+
+// app.delete('/sample/:id',(req,res)=>{
+//     var id = req.params.id
+
+//     if(!ObjectId.isValid(id)){
+//         return res.status(404).send()
+//     }
+//     Todo.findByIdAndRemove(id).then((todos)=>{
+//         if(!todos){
+//             return res.status(404).send()
+//         }
+//         res.send({todos})
+//     }).catch((e)=>{
+//         res.status(400).send()
+//     })
+// })
+
+module.exports = app
